eae9e77596426b2bc244baa1a5cb83dc2ee7a0ec
Matheus Braga <matheusgalaxytab@gmail.com>
2024-07-16 08:39:03 -0400

    First commit - Initial version

---
+++ FinaData.Api/Program.cs
@@ -0,0 +1,25 @@
+using FinaData.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x =>
+{
+    x.CustomSchemaIds(n => n.FullName);
+});
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
