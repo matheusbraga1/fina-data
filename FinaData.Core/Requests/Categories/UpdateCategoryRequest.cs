58b86bd787331a0fa04087bded314099e9582345
Matheus Braga <matheusgalaxytab@gmail.com>
2024-07-16 16:40:26 -0400

    feat: adding id

--- FinaData.Core/Requests/Categories/UpdateCategoryRequest.cs
+++ FinaData.Core/Requests/Categories/UpdateCategoryRequest.cs
@@ -4,6 +4,8 @@ namespace FinaData.Core.Requests.Categories;
 
 public class UpdateCategoryRequest : Request
 {
+    public long Id { get; set; }
+
     [Required(ErrorMessage = "Título inválido")]
     [MaxLength(80, ErrorMessage = "O título deve conter até 80 caracteres")]
     public string Title { get; set; } = string.Empty;
