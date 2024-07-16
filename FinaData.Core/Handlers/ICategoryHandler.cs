39cfcc9dde1e7bb3fe9a46001a859a025d6a3439
Matheus Braga <matheusgalaxytab@gmail.com>
2024-07-16 16:39:47 -0400

    feat: creating category handler contract

---
+++ FinaData.Core/Handlers/ICategoryHandler.cs
@@ -0,0 +1,14 @@
+﻿using FinaData.Core.Models;
+using FinaData.Core.Requests.Categories;
+using FinaData.Core.Responses;
+
+namespace FinaData.Core.Handlers;
+
+public interface ICategoryHandler
+{
+    Task<Response<Category?>> CreateAsync(CreateCategoryRequest request);
+    Task<Response<Category?>> UpdateAsync(UpdateCategoryRequest request);
+    Task<Response<Category?>> DeleteAsync(DeleteCategoryRequest request);
+    Task<Response<Category?>> GetByIdAsync(GetCategoryByIdRequest request);
+    Task<PagedResponse<List<Category>>> GetAllAsync(GetAllCategoriesRequest request);
+}
