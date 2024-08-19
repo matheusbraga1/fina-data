4d7386eae8d2a1aae8c9f83e775fe6561e2a7b47
Matheus Braga <matheusgalaxytab@gmail.com>
2024-08-19 17:06:28 -0400

    feat: adjust program

--- FinaData.Api/Program.cs
+++ FinaData.Api/Program.cs
@@ -2,9 +2,18 @@ using FinaData.Api.Data;
 using FinaData.Api.Endpoints;
 using FinaData.Api.Handlers;
 using FinaData.Core.Handlers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(n => n.FullName); });
+
+builder.Services
+    .AddAuthentication(IdentityConstants.ApplicationScheme)
+    .AddIdentityCookies();
+builder.Services.AddAuthorization();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -15,10 +24,6 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
 
 builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(n => n.FullName); });
 
 var app = builder.Build();
 
