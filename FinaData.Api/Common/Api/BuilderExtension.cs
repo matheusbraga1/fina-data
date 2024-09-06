a2b64ef9560dafc3b72259ac06954c18e3eb8b69
Matheus Braga <matheusgalaxytab@gmail.com>
2024-09-06 13:30:34 -0400

    feat: add cors configuration

--- FinaData.Api/Common/Api/BuilderExtension.cs
+++ FinaData.Api/Common/Api/BuilderExtension.cs
@@ -13,6 +13,9 @@ public static class BuilderExtension
     public static void AddConfiguration(this WebApplicationBuilder builder)
     {
         Configuration.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+        
+        Configuration.BackendUrl = builder.Configuration.GetValue<string>("BackendUrl") ?? string.Empty;
+        Configuration.FrontendUrl = builder.Configuration.GetValue<string>("FrontendUrl") ?? string.Empty;
     }
 
     public static void AddDocumentation(this WebApplicationBuilder builder)
@@ -29,7 +32,16 @@ public static class BuilderExtension
 
     public static void AddCrossOrigin(this WebApplicationBuilder builder)
     {
-
+        builder.Services.AddCors(options => 
+            options.AddPolicy(
+                ApiConfiguration.CorsPolicyName,
+                policy => policy
+                    .WithOrigins([Configuration.BackendUrl, Configuration.FrontendUrl])
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                )
+            );
     }
 
     public static void AddDataContexts(this WebApplicationBuilder builder)
