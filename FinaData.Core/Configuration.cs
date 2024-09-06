815a6feaac686ebf9e45fd5dd3257cb2995a99eb
Matheus Braga <matheusgalaxytab@gmail.com>
2024-09-06 13:31:55 -0400

    feat: add frontend and backend strings configuration

--- FinaData.Core/Configuration.cs
+++ FinaData.Core/Configuration.cs
@@ -7,4 +7,7 @@ public static class Configuration
     public const int DefaultPageNumber = 1;
 
     public static string ConnectionString { get; set; } = string.Empty;
+
+    public static string BackendUrl { get; set; } = string.Empty;
+    public static string FrontendUrl { get; set; } = string.Empty;
 }
