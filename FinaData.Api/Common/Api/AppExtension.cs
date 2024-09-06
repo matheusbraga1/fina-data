ace039f32e3599eb5393331a8abfa08ca4ba8bd7
Matheus Braga <matheusgalaxytab@gmail.com>
2024-09-06 12:52:56 -0400

    feat: add app and builder extensions

---
+++ FinaData.Api/Common/Api/AppExtension.cs
@@ -0,0 +1,21 @@
+﻿using FinaData.Api.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace FinaData.Api.Common.Api;
+
+public static class AppExtension
+{
+    public static void UseSecurity(this WebApplication app)
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
+    }
+
+    public static void ConfigureDevEnvironment(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.MapSwagger().RequireAuthorization();
+    }
+}
---
+++ FinaData.Api/Common/Api/BuilderExtension.cs
@@ -0,0 +1,54 @@
+﻿using FinaData.Api.Data;
+using FinaData.Api.Handlers;
+using FinaData.Api.Models;
+using FinaData.Core;
+using FinaData.Core.Handlers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinaData.Api.Common.Api;
+
+public static class BuilderExtension
+{
+    public static void AddConfiguration(this WebApplicationBuilder builder)
+    {
+        Configuration.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+    }
+
+    public static void AddDocumentation(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(n => n.FullName); });
+    }
+
+    public static void AddSecurity(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme).AddIdentityCookies();
+        builder.Services.AddAuthorization();
+    }
+
+    public static void AddCrossOrigin(this WebApplicationBuilder builder)
+    {
+
+    }
+
+    public static void AddDataContexts(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(Configuration.ConnectionString);
+        });
+        builder.Services
+            .AddIdentityCore<User>()
+            .AddRoles<IdentityRole<long>>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddApiEndpoints();
+    }
+
+    public static void AddServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddTransient<ICategoryHandler, CategoryHandler>();
+
+        builder.Services.AddTransient<ITransactionHandler, TransactionHandler>();
+    }
+}
