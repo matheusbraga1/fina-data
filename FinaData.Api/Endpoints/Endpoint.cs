b6602781fd5717e576587ec642fa4551f5c2b970
Matheus Braga <matheusgalaxytab@gmail.com>
2024-09-06 12:53:59 -0400

    feat: add identity route group

--- FinaData.Api/Endpoints/Endpoint.cs
+++ FinaData.Api/Endpoints/Endpoint.cs
@@ -1,6 +1,8 @@
 ﻿using FinaData.Api.Common.Api;
 using FinaData.Api.Endpoints.Categories;
+using FinaData.Api.Endpoints.Identity;
 using FinaData.Api.Endpoints.Transactions;
+using FinaData.Api.Models;
 
 namespace FinaData.Api.Endpoints;
 
@@ -10,9 +12,13 @@ public static class Endpoint
     {
         var endpoints = app.MapGroup("");
 
+        endpoints.MapGroup("/")
+            .WithTags("Health Check")
+            .MapGet("/", () => new { message = "OK" });
+
         endpoints.MapGroup("v1/categories")
             .WithTags("Categories")
-            // .RequireAuthorization()
+            .RequireAuthorization()
             .MapEndpoint<CreateCategoryEndpoint>()
             .MapEndpoint<UpdateCategoryEndpoint>()
             .MapEndpoint<DeleteCategoryEndpoint>()
@@ -21,12 +27,21 @@ public static class Endpoint
 
         endpoints.MapGroup("v1/transactions")
             .WithTags("Transactions")
-            // .RequireAuthorization()
+            .RequireAuthorization()
             .MapEndpoint<CreateTransactionEndpoint>()
             .MapEndpoint<UpdateTransactionEndpoint>()
             .MapEndpoint<DeleteTransactionEndpoint>()
             .MapEndpoint<GetTransactionByIdEndpoint>()
             .MapEndpoint<GetTransactionByPeriodEndpoint>();
+
+        endpoints.MapGroup("v1/identity")
+            .WithTags("Identity")
+            .MapIdentityApi<User>();
+
+        endpoints.MapGroup("v1/identity")
+            .WithTags("Identity")
+            .MapEndpoint<LogoutEndpoint>()
+            .MapEndpoint<GetRolesEndpoint>();
     }
 
     private static IEndpointRouteBuilder MapEndpoint<TEndpoint>(this IEndpointRouteBuilder app) 
