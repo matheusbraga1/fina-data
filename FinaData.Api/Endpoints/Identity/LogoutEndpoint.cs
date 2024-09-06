f7b76d9e0c5f7f449f39dde86d39334baf1fdfd9
Matheus Braga <matheusgalaxytab@gmail.com>
2024-09-06 12:53:18 -0400

    feat: add identity endpoints

---
+++ FinaData.Api/Endpoints/Identity/LogoutEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using FinaData.Api.Common.Api;
+using FinaData.Api.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace FinaData.Api.Endpoints.Identity;
+
+public class LogoutEndpoint : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+        => app.MapPost("/logout", HandleAsync).RequireAuthorization();
+
+    private static async Task<IResult> HandleAsync(SignInManager<User> signInManager)
+    {
+        await signInManager.SignOutAsync();
+        return Results.Ok();
+    }
+}
