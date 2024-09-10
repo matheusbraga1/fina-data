e291a80855441f0971dd6204439cca2ce4033ae6
Matheus Braga <matheusgalaxytab@gmail.com>
2024-09-10 16:07:21 -0400

    feat: adjusting getRoles endpoint

--- FinaData.Api/Endpoints/Identity/GetRolesEndpoint.cs
+++ FinaData.Api/Endpoints/Identity/GetRolesEndpoint.cs
@@ -1,5 +1,6 @@
 ﻿using FinaData.Api.Common.Api;
 using System.Security.Claims;
+using FinaData.Core.Models.Account;
 
 namespace FinaData.Api.Endpoints.Identity;
 
@@ -16,13 +17,13 @@ public class GetRolesEndpoint : IEndpoint
         var identity = (ClaimsIdentity)user.Identity;
         var roles = identity
             .FindAll(identity.RoleClaimType)
-            .Select(c => new
+            .Select(c => new RoleClaim
             {
-                c.Issuer,
-                c.OriginalIssuer,
-                c.Type,
-                c.Value,
-                c.ValueType
+                Issuer = c.Issuer,
+                OriginalIssuer = c.OriginalIssuer,
+                Type = c.Type,
+                Value = c.Value,
+                ValueType = c.ValueType
             });
 
         return Task.FromResult<IResult>(TypedResults.Ok(roles));
