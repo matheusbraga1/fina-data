3a30239211dcdcf97606b4306de1fa896aa24681
Matheus Braga <matheusgalaxytab@gmail.com>
2024-09-12 16:19:59 -0400

    feat: adjusting dark and light themes

--- src/FinaData.Web/Configuration.cs
+++ src/FinaData.Web/Configuration.cs
@@ -1,4 +1,5 @@
 using MudBlazor;
+using MudBlazor.Utilities;
 
 namespace FinaData.Web;
 
@@ -20,21 +21,22 @@ public static class Configuration
         Palette = new PaletteLight
         {
             Primary = "#1EFA2D",
+            PrimaryContrastText = new MudColor("#000000"),
             Secondary = Colors.LightGreen.Darken3,
             Background = Colors.Grey.Lighten4,
             AppbarBackground = "#1EFA2D",
             AppbarText = Colors.Shades.Black,
             TextPrimary = Colors.Shades.Black,
-            PrimaryContrastText = Colors.Shades.Black,
-            DrawerText = Colors.Shades.Black,
-            DrawerBackground = Colors.LightGreen.Lighten4
+            DrawerText = Colors.Shades.White,
+            DrawerBackground = Colors.Green.Darken4
         },
         PaletteDark = new PaletteDark
         {
             Primary = Colors.LightGreen.Accent3,
             Secondary = Colors.LightGreen.Darken3,
             AppbarBackground = Colors.LightGreen.Accent3,
-            AppbarText = Colors.Shades.Black
+            AppbarText = Colors.Shades.Black,
+            PrimaryContrastText = new MudColor("#000000")
         }
     };
 }
